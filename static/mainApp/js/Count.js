f93eb9f91a97cadff461ac112ea3b90aa457e2d9
Anton2156 <akkforwork11@gmail.com>
2022-01-26 21:50:52 +0200

    [collect static]

--- static/mainApp/js/Count.js
+++ static/mainApp/js/Count.js
@@ -1,6 +1,6 @@
-let goods = document.querySelectorAll('.product-btn');
+let productBtns = document.querySelectorAll('.product-btn');
 
-Array.from(goods).forEach(
+Array.from(productBtns).forEach(
     function (button) {
         button.addEventListener('click', function (e) {
             let productBtn = e.target.parentElement;
@@ -15,7 +15,6 @@ Array.from(goods).forEach(
                 let decrement = productBtn.querySelector(`#product-btn__dec${goodId}`);
                 let count = productBtn.querySelector(`#input${goodId}`);
                 let countValue = Number(count.value);
-                console.log(increment.onclick, decrement.onclick)
 
                 increment.onclick = () => {
                     count.value = countValue + 1;
